f1f28607674d6b41c5ae827a9f6c463609441831
jeff <jeff@Stan.osnetwork>
2008-11-01 05:30:59 -0700

    Added cache system for TVDB queries of the same name
    Cache system consists of a check for whether or not a name has been searched for
    by hashing the name and checking for the existance of that key in the Hash
    $TVDB_CACHE.  At this point, the time of the cache is not tracked.

--- MediaManager.rb
+++ MediaManager.rb
@@ -7,14 +7,17 @@
 $mmconfig = "#{`cd ~;pwd`.chomp}/Documents/Projects/MediaManager/MediaManager.cfg"
 raise "Cannot read config file!!!" unless load $mmconfig
 
+#Initialize the cache arrays
+$TVDB_CACHE={}
+
 #This is so that we can require files without the .rb extention.
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
 
-load 'MMCommon'
+require 'MM_TVDB'
 
 module MediaManager
   extend MMCommon
-  def self.import numFiles=nil
+  def self.import numFiles=:nil
     raise "Sanity check fail!" unless sanity_check
     puts "Indexing files in #{$MEDIA_SOURCES_DIR},\n"
     puts "This may take some time.\n"
@@ -49,82 +52,13 @@ module MediaManager
 
         end # Ignore the file
 				unless numFiles==:nil
-					then numFiles = numFiles+1
+					then numFiles = numFiles-1
 				end
       end # End of finding all files in source_dir
     end #end $MEDIA_SOURCES_DIR.each
 
     puts "Done.\n"
   end
-
-  def filename_to_info(filename)
-    if filename.index('//') || filename.index('\/')
-		  raise "Not yet able to process files with '//' or escaped slashes such as '\/'.\n"
-		end
-
-		is_movie?(filename)
-
-		puts filename.to_s
-	end
-
-	def is_movie?(fp)
-		#Return True or False
-
-	  #Store the results from each assesment attempt
-	  #for reference and recalculation at end of function
-	  answers = []
-
-		#split the path to make it searchable, but retain full path
-		#file_path is array of the seperated names of each parent folder
-		file_path=fp.split('/')
-		counter=0
-
-		#split on '.'
-		#file_path.each do |level|
-		#	if level.index('.')!=nil && level.index('.') < level.length-4
-		#		filename[counter] = level.split('.')
-		#		level=filename[counter]
-		#	end
-
-		#0.
-		#Does the path contain 'movie' in it?
-		file_path.each do |filepath_segment|
-			if filepath_segment.downcase.index("movie")
-				answers[0]= "TRUE"
-			else
-				answers[0]= "FALSE"
-			end
-		end
-
-		#1.
-		#Does the path also contain 'tv' or 'television' in it?
-		file_path.each do |filepath_segment|
-			if filepath_segment.downcase.index("tv")
-				answers[1]= "tv"
-			elsif filepath_segment.downcase.index("television")
-				answers[1]= "television"
-			else
-				answers[1]= "FALSE"
-			end
-		end
-
-		#2.
-		#What size is it? (in bytes)    >= 650 = movie
-		answers[2]= File.size?(fp)
-		if answers[2]==nil
-			print "Warning: File doesn't exist, or has zero size? #{fp}"
-			#raise "Warning: File doesn't exist, or has zero size? #{fp}"
-		end
-
-		#3.
-		#Is it in VIDEO_TS format?
-		fp.index("VIDEO_TS") != nil ? answers[3]=TRUE : answers[3]=FALSE
-
-		#4.
-		#
-
-		pp answers
-	end
 
 end
 
