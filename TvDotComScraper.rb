ad088795cc63a0d2d8dd3b204392e6075c726884
jeff <jeff@Stan.osnetwork>
2009-04-04 13:50:41 -0700

    still learning git branching

--- TvDotComScraper.rb
+++ TvDotComScraper.rb
@@ -5,6 +5,8 @@ require 'mysql'
 require 'timeout'
 
 #another change
+#OMG BRANCH LOOKATME IMA BRANCH
+
 #Configuration variables
 $Database_name='TvDotComScraperCache'
 $Database_host='mysql.osnetwork'
