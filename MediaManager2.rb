893b916c748721f6c1c2a5523e97f51dfb77adf5
Jeff Welling <Jeff.Welling@Gmail.com>
2009-10-29 12:18:58 -0700

    Added lib to loadpath and a require for MMCommon.rb

--- MediaManager2.rb
+++ MediaManager2.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/ruby
+current_dir = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift(current_dir + "/lib")
+require 'MMCommon.rb'
+#Stuff! It happens here!
+#include MediaManager::MMCommon
 
-module MediaManager
-end
