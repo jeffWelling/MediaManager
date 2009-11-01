09276da7986790f43bb1b1dbe9af38995d67360e
Jeff Welling <Jeff.Welling@Gmail.com>
2009-11-01 14:36:36 -0800

    Added a commands dir, and two files in it as an example.

---
+++ lib/commands/import.rb
@@ -0,0 +1,1 @@
+#This file will hold the code for the import part of  the program.
---
+++ lib/commands/remap.rb
@@ -0,0 +1,1 @@
+#This file will hold the code for the part of the program that creates shortcuts from the media to the Library dir.
