a3b5b4cc37036c8bef6f0c3afd52e810f7822380
Jeff Welling <Jeff.Welling@Gmail.com>
2009-11-09 02:14:40 -0800

    Added Storage.rb, meant to be an abstracted interface for multiple optional storage backends (like sql, or a yaml file)

---
+++ lib/MediaManager/Storage.rb
@@ -0,0 +1,29 @@
+=begin
+  Copyright 2009, Jeff Welling
+
+    This file is part of MediaManager.
+
+    MediaManager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaManager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaManager.  If not, see <http://www.gnu.org/licenses/>.
+  
+=end
+module MediaManager
+  #This class is meant to be the abstracted interface to the information storage system
+  #Instead of coding specifically for a SQL database or YAML file as a database backend,
+  #This class will be used, which will decide wether to use sql or whatnot based on configs
+  class Storage
+    class << self
+      
+    end
+  end
+end
