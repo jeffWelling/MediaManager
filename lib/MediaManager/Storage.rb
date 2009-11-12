26aecd76f663f9902ad32173638349af935c1a92
Jeff Welling <Jeff.Welling@Gmail.com>
2009-11-12 12:15:30 -0800

    Beginning implementing sqlite/mysql support in Storage.rb

--- lib/MediaManager/Storage.rb
+++ lib/MediaManager/Storage.rb
@@ -21,7 +21,13 @@ module MediaManager
   #This class is meant to be the abstracted interface to the information storage system
   #Instead of coding specifically for a SQL database or YAML file as a database backend,
   #This class will be used, which will decide wether to use sql or whatnot based on configs
+  $using=:sqlite       #One of either :sqlite, :mysql, or :yaml
   $yaml_import_list='~/.mmanager/import_list.yaml'
+  $sqlite_file='~/.mmanager/mmanager.sqlite'
+  $sql_database='mmanager'  #We will assume the database is already created, by jebus or someone.
+  $mysql_host='mysql'
+  $mysql_user='omgwtfbbqsqluser'
+  $mysql_pass='omgwtfbbqsqlpass'
   class Storage
     class << self
       def saveToYaml paths
@@ -29,11 +35,14 @@ module MediaManager
       end 
       #Take paths, an array of paths, and put it the backend
       def importPaths paths
-        saveToYaml(paths)
+        $using==:sqlite ? saveToYaml(paths) : savePathsToSql(paths)
       end
       #Sql method to import paths to sql database, swappable with the saveToYaml method
-      def savePathsToSql
+      def savePathsToSql paths
         
+      end
+      def sqlConnect
+        $using==:sqlite ? DBI.connect( ) : DBI.connect("dbi:Mysql:mmanager:#{$mysql_host}", $mysql_user, $mysql_pass)
       end
     end
   end
