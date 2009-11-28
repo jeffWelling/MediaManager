12ecdcf992fe27e295532b2d016633ebd50b680e
Jeff Welling <Jeff.Welling@Gmail.com>
2009-11-28 13:54:18 -0800

    Moved example scraper into an appropriatly named file

--- lib/MediaManager/scrapers/example_scraper.rb
+++ lib/MediaManager/scrapers/example_scraper.rb
@@ -15,16 +15,19 @@
 
     You should have received a copy of the GNU General Public License
     along with MediaManager.  If not, see <http://www.gnu.org/licenses/>.
-  
 =end
-module MediaManager
-  module Scrapers
-    module TestScraper
-      class << self
-        def doit
-          puts 'jesusfuck'
-        end
-      end
-    end
-  end
-end
+
+#Example scraper module
+#module MediaManager
+#  module Scrapers
+#    module TestScraper
+#      class << self
+#        #Accessible in irb as MediaManager::Scrapers::TestScraper.doit
+#        def doit
+#          puts 'jesusfuck'
+#        end
+#      end
+#    end
+#  end
+#end
+
