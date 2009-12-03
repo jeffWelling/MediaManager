94c8177294daacccffff806fe6f20cb7ed34fe97
Jeff Welling <Jeff.Welling@Gmail.com>
2009-12-03 02:50:11 -0800

    Began writing thetvdbscraper.rb scraper, to interface with libthetvdb

---
+++ lib/MediaManager/scrapers/thetvdbscraper.rb
@@ -0,0 +1,37 @@
+=begin
+  Copyright 2009, Jeff Welling
+
+    This file is part of MediaManager.
+
+    MediaManager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaManager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaManager.  If not, see <http://www.gnu.org/licenses/>.
+=end
+#Example scraper module
+module MediaManager
+  module Scrapers
+    #Note that the name of the module is the same as the 
+    module Thetvdbscraper
+      #All scrapers should have a search() method
+      #This search method should return an array of MediaFiles (or decendants)
+      #MediaManager::Media::MediaFile
+      def search str
+        require 'pp'
+        results= [Thetvdb.search(str)]
+        return [] if results[0].class==Hash and results[0].empty?
+        details= Thetvdb.formatTvdbResults results
+        
+        results
+      end
+    end
+  end
+end
