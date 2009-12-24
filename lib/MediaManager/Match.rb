39456aec0a4a1316775b137732d9486db6ba6139
Jeff Welling <Jeff.Welling@Gmail.com>
2009-12-24 12:32:05 -0800

    Began Match.rb, contains pattern matching code from before the redesign. (yet to be filled in)

---
+++ lib/MediaManager/Match.rb
@@ -0,0 +1,27 @@
+=begin
+  Copyright 2009, Jeff Welling
+
+    This file is part of MediaManager.
+
+    MediaManager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaManager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaManager.  If not, see <http://www.gnu.org/licenses/>.
+  
+=end
+module MediaManager
+  #The Match module contains methods for advanced pattern matching
+  #intended for use matching filenames to movie information to
+  #id files.
+  module Match
+    
+  end
+end
