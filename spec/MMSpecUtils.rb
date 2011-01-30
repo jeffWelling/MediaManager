9a107df5988564746863f16f0722d14f6355fea7
Jeff Welling <Jeff.Welling@Gmail.com>
2011-01-30 18:03:24 -0500

    Added comments to MMSpecUtils methods

--- spec/MMSpecUtils.rb
+++ spec/MMSpecUtils.rb
@@ -3,12 +3,15 @@ require 'fileutils'
 
 module MMSpecUtils
   #scans a dir, and saves all of the path names to a file
-  def self.scanDir to_scan, output_file='output.txt'
+  #Intended use is for taking a snapshot of existing raw,
+  #freshly downloaded file names, for use match testing
+  def self.scanDir to_scan, output_file='scanned_list.txt'
     puts "Searching for #{to_scan}"
     Dir.glob( to_scan ) {|path| puts path}
   end
 end
 
+#If there are arguments, pass them, otherwise don't.
 $*==[] ? MMSpecUtils.scanDir : MMSpecUtils.scanDir($*)
 
 #If MMSpecUtils is being run, not simply loaded
